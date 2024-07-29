68921d459b9cab76ca57152cfe748d403020c026
Lxdovic <ludovicdebever0@gmail.com>
2024-07-29 11:49:03 +0200

    feat(tokenizer): add tokenization

--- FTSearchEngine/Program.cs
+++ FTSearchEngine/Program.cs
@@ -1,8 +1,12 @@
-﻿namespace FTSearchEngine;
+﻿using static FTSearchEngine.TokenizerNS.Tokenizer;
 
+namespace FTSearchEngine;
 
 public class Program {
     public static void Main() {
-        Levenshtein.Distance("niche", "chien");
+        var tokens = Tokenize("english", "It's alive! It's alive!");
+
+        foreach (var token in tokens)
+            Console.WriteLine(token);
     }
 }

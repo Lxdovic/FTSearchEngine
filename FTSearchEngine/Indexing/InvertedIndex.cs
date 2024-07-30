02284ff7094e2c373b94b6440679fed3f7ea004d
Lxdovic <ludovicdebever0@gmail.com>
2024-07-30 13:37:26 +0200

    improv(indexing): improved inverted index

--- FTSearchEngine/Indexing/InvertedIndex.cs
+++ FTSearchEngine/Indexing/InvertedIndex.cs
@@ -2,17 +2,9 @@ using FTSearchEngine.Tokenizing;
 
 namespace FTSearchEngine.Indexing;
 
-public class WordIndex {
-    public List<int> DocumentIds { get; } = new();
-
-    public void AddDocumentId(int documentId) {
-        DocumentIds.Add(documentId);
-    }
-}
-
 public class InvertedIndex {
     public List<string> Documents = new();
-    public Dictionary<string, WordIndex> Indices = new();
+    public Dictionary<string, List<int>> Indices = new();
 
     public void IndexDocument(string document) {
         var tokens = Tokenizer.Tokenize("english", document);
@@ -21,19 +13,19 @@ public class InvertedIndex {
 
         foreach (var token in tokens) {
             if (!Indices.ContainsKey(token))
-                Indices[token] = new WordIndex();
+                Indices[token] = new List<int>();
 
-            Indices[token].AddDocumentId(Documents.Count);
+            Indices[token].Add(Documents.Count);
         }
     }
 
-    public List<(string word, WordIndex index)> Search(string query) {
+    public List<(string word, List<string> documents)> Search(string query) {
         var tokens = Tokenizer.Tokenize("english", query);
-        var results = new List<(string word, WordIndex index)>();
+        var results = new List<(string word, List<string> documents)>();
 
         foreach (var token in tokens)
             if (Indices.ContainsKey(token))
-                results.Add((token, Indices[token]));
+                results.Add((token, Indices[token].Select(index => Documents[index - 1]).ToList()));
 
         return results;
     }
--- FTSearchEngine/Program.cs
+++ FTSearchEngine/Program.cs
@@ -4,8 +4,20 @@ namespace FTSearchEngine;
 
 public static class Program {
     public static void Main() {
-        var tokens = Tokenizer.Tokenize("french", "continu continua continuait continuant continuation");
-
-        foreach (var token in tokens) Console.WriteLine(token);
+        var index = new Indexing.InvertedIndex();
+        
+        index.IndexDocument("The quick brown fox jumps over the lazy dog.");
+        index.IndexDocument("The quick brown fox jumps over the lazy dog and the lazy cat.");
+        index.IndexDocument("The quick brown fox jumps over the lazy dog and the lazy cat. The quick brown fox jumps over the lazy dog and the lazy cat.");
+        
+        var results = index.Search("cat");
+        
+        foreach (var (word, documents) in results) {
+            Console.WriteLine($"Word: {word}");
+            Console.WriteLine("Documents:");
+            foreach (var document in documents) {
+                Console.WriteLine(document);
+            }
+        }
     }
 }
